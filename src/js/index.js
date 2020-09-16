b318c278a8e0c24529f013c3ce1c16c38033f07f
Ilya Chernetskiy <info@smarto.pro>
2020-09-16 15:37:17 +0300

    createStore done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 import "../scss/index.scss";
-import { Excel } from "@/components/excel/excel";
-import { Header } from "@/components/header/Header";
-import { Toolbar } from "@/components/toolbar/Toolbar";
-import { Formula } from "@/components/formula/Formula";
-import { Table } from "@/components/table/Table";
+import { Excel } from "@/components/excel/excel.js";
+import { Header } from "@/components/header/Header.js";
+import { Toolbar } from "@/components/toolbar/Toolbar.js";
+import { Formula } from "@/components/formula/Formula.js";
+import { Table } from "@/components/table/Table.js";
+import { createStore } from "@core/createStore.js";
+import { rootReducer } from "@/redux/rootReducer.js";
+
+const store = createStore(rootReducer());
 
 const excel = new Excel("#app", {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 });
 
 excel.render();
